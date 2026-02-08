ed381f6774fd95aedcdf732c9f60e52a4f6c6dc8
Zeynel Yıldırım <zeynel.yildirim@ug.bilkent.edu.tr>
2026-02-08 17:01:49 +0300

    dice game updated

--- Dicee/index.js
+++ Dicee/index.js
@@ -1,17 +1,62 @@
-var randomNo1 = Math.floor(Math.random()*6 + 1);
-var randomImg1 = "./images/dice" + randomNo1 + ".png";
-var randomNo2 = Math.floor(Math.random()*6 + 1);
-var randomImg2 = "./images/dice" + randomNo2 + ".png";
+rollDice();
 
-document.querySelector(".img1").setAttribute("src", randomImg1);
-document.querySelector(".img2").setAttribute("src", randomImg2);
+document.querySelector(".btn").addEventListener("click", function () {
+  buttonAnimation();
+  rollDice();
+});
 
-var title = document.querySelector("h1");
+document.addEventListener("keydown", function (event) {
+  buttonAnimation();
+  var btn = event.key;
+  if (btn === "Enter" || btn === " ") {
+    rollDice();
+  }
+});
 
-if(randomNo1>randomNo2){
-    title.textContent = "🚩Player 1 Wins"
-} else if(randomNo2>randomNo1){
-    title.textContent = "Player 2 Wins🚩"
-} else{
-    title.textContent = "TIE!"
+function rollDice() {
+  var title = document.querySelector("h1");
+
+  var btn = document.querySelector(".btn");
+  var randomNo1 = Math.floor(Math.random() * 6 + 1);
+  var randomImg1 = "./images/dice" + randomNo1 + ".png";
+  var randomNo2 = Math.floor(Math.random() * 6 + 1);
+  var randomImg2 = "./images/dice" + randomNo2 + ".png";
+
+  document.querySelector(".img1").setAttribute("src", randomImg1);
+  document.querySelector(".img2").setAttribute("src", randomImg2);
+
+  if (randomNo1 > randomNo2) {
+    title.textContent = "🚩Player 1 Wins";
+  } else if (randomNo2 > randomNo1) {
+    title.textContent = "Player 2 Wins🚩";
+  } else {
+    title.textContent = "TIE!";
+  }
+
+  setTimeout(function () {
+    if (
+      (randomNo1 === 2 && randomNo2 === 4) ||
+      (randomNo2 === 2 && randomNo1 === 4)
+    ) {
+      document.body.style.backgroundImage =
+        "url('./images/heart_background.png')";
+
+      setTimeout(function () {
+        alert("❤️Seni çok seviyoruum❤️");
+      }, 100);
+
+      setTimeout(function () {
+        document.body.style.backgroundImage = "none";
+      }, 3000);
+    }
+  }, 50);
+}
+
+function buttonAnimation() {
+  var activeButton = document.querySelector(".btn");
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
